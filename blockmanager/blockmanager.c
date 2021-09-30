6f4d21d343c8f9872aedabe69402adbb709f8b5b
guilherme.092011 <guilherme.092011@hotmail.com>
2021-09-30 20:37:05 -0300

    reu rivalino

--- blockmanager/blockmanager.c
+++ blockmanager/blockmanager.c
@@ -3,15 +3,10 @@
 #include <fcntl.h>
 #include "blockmanager.h"
 
-/* 
-4000 -> 5000
-5000 - 4000 = 1000
-block_num * BLOCK_SIZE - block_actual_size * BLOCK_SIZE 
-*/
-
 int read_block(int pen_fd, int block_num, void* buf) {
   // go to block
-  if(lseek(pen_fd, (off_t) block_num * BLOCK_SIZE, SEEK_SET))
+  off_t actual = lseek(pen_fd, 0, SEEK_CUR);
+  if(lseek(pen_fd, (off_t) (block_num * BLOCK_SIZE) - (actual * BLOCK_SIZE), SEEK_SET))
     return -1;
   // reads from pen_fd and writes to buf 'BLOCK_SIZE' bytes
   return read(pen_fd, buf, BLOCK_SIZE);
@@ -19,7 +14,8 @@ int read_block(int pen_fd, int block_num, void* buf) {
 
 int write_block(int pen_fd, int block_num, void* buf) {
   // go to block
-  if(lseek(pen_fd, (off_t) block_num * BLOCK_SIZE, SEEK_SET))
+  off_t actual = lseek(pen_fd, 0, SEEK_CUR);
+  if(lseek(pen_fd, (off_t) (block_num * BLOCK_SIZE) - (actual * BLOCK_SIZE), SEEK_SET))
     return -1;
   // reads from buf and writes to pen_fd 'BLOCK_SIZE' bytes
   return write(pen_fd, buf, BLOCK_SIZE);
---
+++ ufufs/ufufs.h
@@ -0,0 +1,9 @@
+
+// SHARED LIB
+
+void ufufs_create();
+void ufufs_open();
+void ufufs_read();
+void ufufs_write();
+void ufufs_close();
+void ufufs_seek();
