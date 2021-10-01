a0541ba7d0be1fc2b0a308a7dd9613c178c839dd
Pedro Leale <pedroleale@gmail.com>
2021-10-01 08:42:21 -0300

    create shell files

---
+++ ufufsShell/shell.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define clear() printf("\033[H\033[J")
+/*
+    A ideia do algoritmo é: ler o input, procurar palavras definidas na tabelahash do shell
+    utilizando uma busca, para facilitar vou reutilizar o código do nosso tra2 de aed2
+    se reconhecer alguma palavra executar função dela
+*/
+
+
+int main(){
+    
+    /*
+        le a string, até achar um espaço, então procura a palavra na tabela
+        se achar a palavra chama uma função que executa o respectivo comando, se não 
+        retorna erro e pede para ler dnv
+    */
+}
+
+void inicia_shell(){
+    /*
+        insere as palavras na tabela, ainda estou em duvida de como farei as chaves
+        vou dar commit só pra deixar organizado o gith
+    */
+    printf("Bem vindo ao mini-shell do UFU file system\n");
+}
---
+++ ufufsShell/shelltable.h
@@ -0,0 +1,1 @@
+typedef struct hash Hash;
