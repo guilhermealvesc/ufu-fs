3cf3f18bde649205f761273b4c590d041ff76be3
guilherme.092011 <guilherme.092011@hotmail.com>
2021-10-06 13:07:08 -0300

    conversa joao 6/9

--- ufufs/ufufs.c
+++ ufufs/ufufs.c
@@ -2,24 +2,36 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include "./ufufs.h"
+#define MAX_FDS 30
+
 // usa o fd do SO
-int penFd = -1;
+typedef struct {
+  int inode;
+  int qntBlocks;
+  int* blocosArquivo; //[block, block, block]
+  unsigned int offset;
+} FD;
+
+typedef struct {
+  int penFd;
+  // int BYTES;
+  // int BLOCKS;
+  FD* fds[MAX_FDS];
+} MountData;
+
+//[0]
+MountData md = {-1, NULL};
 // todas rotinas devem verificar se o fs foi montado
 
 // vetor de file descriptors, lib gerencia arquivos abertor pelos fd's
 
-struct fileDesc {
-  int inode;
-  int qntBlocks;
-  int* blocosArquivo;
-  unsigned int offset;
-};
+
 // int       int    int         vet    vet
 // [MAGIC_N, BYTES, BLOCK_SIZE, FILES, FAT]
 int ufufs_mount(const char* filePath) {
   //tenta abrir o pen drive 
   //atribui o fd na abertura para var global
-  penFd = open(filePath, O_RDWR);
+  md.penFd = open(filePath, O_RDWR);
   if(penFd < 0) return 0; //adicionar errno
   // read(penFd, );
   //puxar os arquivos importantes, FAT, METADADOS, INFORMAÇÕES
@@ -36,10 +48,17 @@ int ufufs_mount(const char* filePath) {
 
 FileDescriptor ufufs_open(const char* filename) {
   // olha na tabela de metadados se o arquivo existe 
+  
   // existindo, criar o ponteiro para arquivo inicializado com deslocamento em bytes zerado (offset),
+  int fd;
+  for(fd = 0; fd < MAX_FDS; fd++) {
+    if(!md.fds[fd]) break;
+  }
+
   // tabela de metadados tem a quantidade de bytes do arquivo
   // sabendo disso e do tamanho do block size, alocar vetor com tamanho GET_BLOCKS(bytes_file) para comportar arquivo
   // retorna um inteiro (chave) para acessar arquivo
+  return fd;
 }
 
 int ufufs_read(FileDescriptor fd, void *buf, size_t count) {
@@ -57,8 +76,12 @@ int ufufs_write(FileDescriptor fd, void *buf, size_t count) {
 
 
 int ufufs_seek(FileDescriptor fd, int offset) {
+
+  // validar entradas
+  md.fds[fd]->offset += offset;
   // entra na estrutura do fd gerenciado pela lib
   // desloca o offset em 'offset' bytes do parametro
+  return ;
 }
 
 int ufufs_close(FileDescriptor fd) {
