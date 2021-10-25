3025034d1197cc04b7aa641f577d40d5d955652b
Pedro Leale <pedroleale@gmail.com>
2021-10-25 15:34:02 -0300

    Update shellTADheader.h

--- ufufsShell/shellTADheader.h
+++ ufufsShell/shellTADheader.h
@@ -4,7 +4,6 @@
 
 typedef unsigned int FileDescriptor;
 
-//int formatCommand(char *str);
 char **_getArg(char *line);
 char *_readLine();
 char *_getCommand(char *str);
@@ -14,8 +13,15 @@ void cls();
 int cp(char *src, char *dest);
 void ls();
 void helpShell();
-void Donut();
 int Shell_create(char *arg1);
 int Shell_mount(char *arg1);
 FileDescriptor Shell_open(char *arg1);
 void Shell_close(FileDescriptor fd);
+
+
+
+
+
+
+
+//int formatCommand(char *str);
